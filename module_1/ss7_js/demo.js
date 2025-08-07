a1f9ab02c1823dcadb839346fda981570192f373
haitan2408 <haitan28102408@gmail.com>
2025-08-07 19:48:58 +0700

    up

---
+++ module_1/ss7_js/demo.js
@@ -0,0 +1,13 @@
+/*
+ description:
+ author:
+ param
+ return
+*/
+
+document.getElementById("btn-like").addEventListener("click", function () {
+    this.style.backgroundColor='blue';
+    this.style.color='white';
+    this.innerHTML='<b>Đã thích</b>';
+    document.write("Chào bạn")
+})
